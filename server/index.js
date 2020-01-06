2a0aacca6c4aa64fe90d7bab31d0fae164713e3b
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-05 17:36:26 -0800

    final touches on server setup

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 const PORT = 80;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/../public/dist'))
+app.use(express.static(__dirname + '/../client/dist'))
 
 //crud route references...
 
